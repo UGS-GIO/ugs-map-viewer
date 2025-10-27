47d40fc52a5a016dc4d72aefe6604cf84f04e35c
Lance Weaver <lanceweaver@utah.gov>
2025-10-27 13:19:19 -0600

    refactor(wetlandplants): initial setup for wetland plants app (#320)
    * refactor(wetlandplants): initial setup for wetland plants app
    
    * refactor(wetlandplants): deleted unused file, and updated page info
    
    * refactor(wetlandplants): fixes to routing issues
    
    * refactor(wetlandplants): changes to map details, data sources and map layers
    
    * refactor(wetlandplants): deleting unused variables

---
+++ src/pages/wetlandplants/data/layers/layers.tsx
@@ -0,0 +1,49 @@
+import { PROD_GEOSERVER_URL, HAZARDS_WORKSPACE } from "@/lib/constants";
+import { LayerProps, WMSLayerProps } from "@/lib/types/mapping-types";
+//import GeoJSON from "geojson";
+
+
+const studyAreasLayerName = 'studyareas_current';
+const studyAreasWMSTitle = 'Lorem Ipsum';
+const studyAreasWMSConfig: WMSLayerProps = {
+    type: 'wms',
+    url: `${PROD_GEOSERVER_URL}/wms`,
+    title: studyAreasWMSTitle,
+    visible: true,
+    sublayers: [
+        {
+            name: `${HAZARDS_WORKSPACE}:${studyAreasLayerName}`,
+            popupEnabled: false,
+            queryable: true,
+            popupFields: {
+                'Name': { field: 'name', type: 'string' },
+                'Report ID': { field: 'repor_id', type: 'string' },
+                'Mapped Hazards': { field: 'hazard_name', type: 'string' },
+            },
+            linkFields: {
+                'repor_id': {
+                    baseUrl: '',
+                    transform: (value: string) => {
+                        const values = value.split(','); // Split the input value by a comma
+                        const transformedValues = values.map(val => {
+                            const trimmedVal = val.trim();
+                            const href = /^\d+$/.test(trimmedVal)
+                                ? `https://geodata.geology.utah.gov/pages/view.php?ref=${trimmedVal}`
+                                : `https://doi.org/10.34191/${trimmedVal}`;
+                            const label = trimmedVal;
+                            return { label, href };
+                        });
+
+                        return transformedValues;
+                    }
+                }
+            }
+        },
+    ],
+}
+
+const layersConfig: LayerProps[] = [
+    studyAreasWMSConfig
+];
+
+export default layersConfig;
