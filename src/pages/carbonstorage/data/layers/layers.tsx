e530d74948abe72abf06380cfad0381522e0f8c2
lance-weaver <lanceweaver@utah.gov>
2025-10-30 17:32:15 -0600

    refactor(ccs): updates to popup fields

--- src/pages/carbonstorage/data/layers/layers.tsx
+++ src/pages/carbonstorage/data/layers/layers.tsx
@@ -12,7 +12,7 @@ const basinNamesWMSConfig: WMSLayerProps = {
     url: `${PROD_GEOSERVER_URL}/wms`,
     title: basinNamesWMSTitle,
     visible: true,
-    opacity: 0.4,
+    opacity: 0.3,
     sublayers: [
         {
             name: `${ENERGY_MINERALS_WORKSPACE}:${basinNamesLayerName}`,
@@ -333,7 +333,12 @@ const wellWithTopsWMSConfig: WMSLayerProps = {
             queryable: true,
             popupFields: {
                 'API': { field: 'api', type: 'string' },
-                'Well Name': { field: 'wellname', type: 'string' }
+                'Well Name': { field: 'wellname', type: 'string' },
+                'Disclaimer': {
+                    field: 'Formation Tops Disclaimer',
+                    type: 'custom',
+                    transform: () => 'Formation tops and LAS file availability is provided as-is and may not be fully complete or accurate'
+                }
             },
             relatedTables: [
                 {
@@ -377,7 +382,7 @@ const wellWithTopsWMSConfig: WMSLayerProps = {
                                 }
                                 return value !== '' ? value : 'No Data';
                             }
-                        },
+                        }
                     ]
                 }
             ]
@@ -574,31 +579,7 @@ const coresAndCuttingsWMSConfig: WMSLayerProps = {
                     type: 'custom',
                     transform: () => 'Utah Core Research Center Inventory'
                 },
-                'Disclaimer': {
-                    field: 'disclaimer',
-                    type: 'custom',
-                    transform: () => 'Formation tops and LAS file availability is provided \'as is\' and may not be fully complete or accurate'
-                }
             },
-            relatedTables: [
-                {
-                    fieldLabel: 'Formation Tops',
-                    matchingField: 'api',
-                    targetField: 'apishort',
-                    url: PROD_POSTGREST_URL + '/view_wellswithtops_hascore',
-                    headers: {
-                        "Accept-Profile": 'emp',
-                        "Accept": "application/json",
-                        "Cache-Control": "no-cache",
-                    },
-                    displayFields: [
-                        { field: 'formation_alias', label: 'Formation Name' },
-                        { field: 'formation_depth', label: 'Formation Depth (ft)' },
-                    ],
-                    sortBy: 'formation_depth',
-                    sortDirection: 'asc'
-                },
-            ],
             linkFields: {
                 'inventory_link': {
                     transform: (value) => {
