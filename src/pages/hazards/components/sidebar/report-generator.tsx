12d14d3f3dcb312a9460e954a49d1686b2b3ea0f
Clinton Lunn <cpl09c@gmail.com>
2025-11-20 15:18:50 -0700

    feat(hazards): update report text

--- src/pages/hazards/components/sidebar/report-generator.tsx
+++ src/pages/hazards/components/sidebar/report-generator.tsx
@@ -253,16 +253,16 @@ function ReportGenerator() {
                     </p>
                 </div>
                 <div className="space-y-2">
-                    <div className="flex flex-wrap justify-start items-center md:space-x-4">
-                        <Button onClick={handleCurrentMapExtentButton} variant="default" className="w-full md:w-auto flex-grow mb-2 md:mb-0">
+                    <div className="flex flex-wrap gap-2 justify-start items-center">
+                        <Button onClick={handleCurrentMapExtentButton} variant="default" className="w-full md:w-auto flex-grow">
                             {buttonText('currentMapExtent', 'Current Map Extent')}
                         </Button>
-                        <Button onClick={handleCustomAreaButton} variant="default" className="w-full md:w-auto flex-grow mb-2 md:mb-0">
+                        <Button onClick={handleCustomAreaButton} variant="default" className="w-full md:w-auto flex-grow">
                             {buttonText('customArea', 'Draw Custom Area')}
                         </Button>
                     </div>
                     <div className="flex w-full">
-                        <Button onClick={handleReset} variant="secondary" className="w-full flex-grow mb-2 md:mb-0">
+                        <Button onClick={handleReset} variant="secondary" className="w-full flex-grow">
                             {buttonText('reset', 'Reset')}
                         </Button>
                     </div>
