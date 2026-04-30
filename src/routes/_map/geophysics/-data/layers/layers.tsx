327f40832bbbd694525de1ad2c0ba7b75d07d898
Lance Weaver <lanceweaver@utah.gov>
2026-04-30 14:51:30 -0600

    refactor(geophysics): explicity allow only dataqualities of 1, 2, add type label formatting (#428)
    * refactor(geophysics): explicity allow only dataqualities of 1 2 or 3
    
    * refactor(geophysics): deleted 3 as an option leaving only 1 and 2
    
    * refactor(geophysics): Add the well or spring TYPE to popups on both geophysics and carbonstorage
    
    * refactor(geophysics): capitalization of well and spring

--- src/routes/_map/geophysics/-data/layers/layers.tsx
+++ src/routes/_map/geophysics/-data/layers/layers.tsx
@@ -270,6 +270,15 @@ const geothermalWellsWMSConfig: WMSLayerProps = {
             popupEnabled: false,
             queryable: true,
             popupFields: {
+                'Type': {
+                    field: 'type',
+                    type: 'string',
+                    transform: (value) => {
+                        if (value === 'W') return 'Well'
+                        if (value === 'S') return 'Spring'
+                        return value
+                    }
+                },
                 'Map Number': { field: 'mapno', type: 'string' },
                 'Region': {
                     field: 'custom',
@@ -294,7 +303,6 @@ const geothermalWellsWMSConfig: WMSLayerProps = {
                 },
                 'Well/Spring Name': { field: 'source', type: 'string' },
                 'UGS Name': { field: 'idname', type: 'string' },
-                'Type': { field: 'type', type: 'string' },
                 'Temperature': {
                     field: 'custom',
                     type: 'custom',
@@ -573,7 +581,7 @@ const geothermalTEMLayerConfig: WMSLayerProps = {
     url: `${PROD_GEOSERVER_URL}/wms`,
     title: geothermalTEMLayerTitle,
     visible: true,
-    customLayerParameters: { cql_filter: "dataquality <> '3'" },
+    customLayerParameters: { cql_filter: "dataquality IN ('1','2')" },
     sublayers: [
         {
             name: `${ENERGY_MINERALS_WORKSPACE}:${geothermalTEMLayerName}`,
