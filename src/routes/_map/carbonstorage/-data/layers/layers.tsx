f02c9c9042aad3c993e314d98e781b9af4853267
Clinton Lunn <cpl09c@gmail.com>
2026-04-30 17:55:25 -0600

    fix(ccs): bump CO2 sources opacity back to 0.8

--- src/routes/_map/carbonstorage/-data/layers/layers.tsx
+++ src/routes/_map/carbonstorage/-data/layers/layers.tsx
@@ -639,7 +639,7 @@ const co2SourcesWFSConfig: WFSLayerProps = {
     typeName: `${ENERGY_MINERALS_WORKSPACE}:${co2SourcesLayerName}`,
     title: co2SourcesWFSTitle,
     visible: false,
-    opacity: 0.6,
+    opacity: 0.8,
     crs: 'EPSG:4326',
     geometryType: 'point',
     style: {
