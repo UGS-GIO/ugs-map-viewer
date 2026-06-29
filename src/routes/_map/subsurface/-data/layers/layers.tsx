70499dcbabe76940022fc8b100ca0a55f2645c59
lance-weaver <lanceweaver@utah.gov>
2026-06-29 17:20:36 -0600

    refactor(subsurface): added kelly bushing elevation field

--- src/routes/_map/subsurface/-data/layers/layers.tsx
+++ src/routes/_map/subsurface/-data/layers/layers.tsx
@@ -494,6 +494,15 @@ const ucrcWellsWFSConfig: PMTilesLayerProps = {
                         return val;
                     }
                 },
+                'Kelly bushing Elevation (GL ft)': {
+                    field: 'elevation_kb',
+                    type: 'custom',
+                    transform: (properties) => {
+                        const val = properties?.['elevation_kb'];
+                        if (val === null || val === undefined || val === 0 || val === '0') return null;
+                        return val;
+                    }
+                },
                 'Latitude': { field: 'latitude', type: 'number' },
                 'Longitude': { field: 'longitude', type: 'number' },
                 'Easting (NAD83)': {
