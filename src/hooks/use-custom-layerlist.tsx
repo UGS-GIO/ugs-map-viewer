ac526f3130b33f81200c63ac582d88cdf83f5bbe
Clinton Lunn <cpl09c@gmail.com>
2026-08-18 10:04:23 -0600

    feat(common): render layer subtitles on group headers too

--- src/hooks/use-custom-layerlist.tsx
+++ src/hooks/use-custom-layerlist.tsx
@@ -253,9 +253,16 @@ const LayerAccordionItem = ({ layerConfig, isTopLevel, disableExport, groupExtra
                                 className="mx-2"
                             />
                             <AccordionTrigger>
-                                <h3 className="font-medium text-left text-md">
-                                    {layerConfig.title}
-                                </h3>
+                                <div className="text-left">
+                                    <h3 className="font-medium text-md">
+                                        {layerConfig.title}
+                                    </h3>
+                                    {layerConfig.subtitle && (
+                                        <p className="text-xs font-normal text-muted-foreground">
+                                            {layerConfig.subtitle}
+                                        </p>
+                                    )}
+                                </div>
                             </AccordionTrigger>
                         </AccordionHeader>
                         <AccordionContent>
